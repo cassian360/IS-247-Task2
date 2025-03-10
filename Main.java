290a930efbcf968b0ebe8539b690fcedfafa421c
yashphogat <yashp2@umbc.edu>
2025-03-10 18:28:59 -0400

    update

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class Main{
   public static void main(String[] args) {
@@ -7,7 +7,14 @@ public class Main{
     fruits.add("Banana");
     fruits.add("Cherry");
     fruits.add("Date");
+    fruits.add("Banana");
+    fruits.add("Apple");
     Iterator<String> iterator = fruits.iterator();
+
+    while(iterator.hasNext())
+    {
+      System.out.println(iterator.next());
+    }
     
   }
 }
